088a2c811de7c39e568aba99a23842cb7796e00b
Michael Chierici <michael.chierjr@gmail.com>
2022-08-24 20:37:53 -0300

    create theme and globalstyles

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Home from "./src/screens/Home";
+import { ThemeProvider } from "styled-components";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import theme from "./src/screens/Home/styles/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
