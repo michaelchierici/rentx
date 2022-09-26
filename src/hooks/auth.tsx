f136ac11f8a83c8ea74b51f1cf11754538969643
Michael Chierici <michael.chierjr@gmail.com>
2022-09-26 12:01:10 -0300

    fix watermelondb error when start the project and set data in db

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,6 @@
 import React, { createContext, useState, useContext, ReactNode } from "react";
+import { database } from "../databases";
+import { User as ModelUser } from "../databases/models/User";
 import { api } from "../services/api";
 
 interface User {
@@ -34,15 +36,31 @@ function AuthProvider({ children }: AuthProviderProps) {
   const [data, setData] = useState<AuthState>({} as AuthState);
 
   async function signIn({ email, password }: LoginCredentials) {
-    const response = await api.post("/sessions", {
-      email,
-      password,
-    });
-    const { user, token } = response.data;
+    try {
+      const response = await api.post("/sessions", {
+        email,
+        password,
+      });
+      const { user, token } = response.data;
 
-    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-    setData({ token, user });
+      const userCollection = database.get<ModelUser>("users");
+      await database.write(async () => {
+        await userCollection.create((newUser) => {
+          newUser.user_id = user.id;
+          newUser.name = user.name;
+          newUser.email = user.email;
+          newUser.driver_license = user.driver_license;
+          newUser.avatar = user.avatar;
+          newUser.token = token;
+        });
+      });
+
+      setData({ token, user });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
